afdcbf38943552712ca3dce05d3b312e11065584
YashRathod123 <159521313+YashRathod123@users.noreply.github.com>
2024-02-09 14:02:15 +0530

    Update login.jsx

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 
   async function handleLogin() {
     try {
-      const response = await fetch("http://localhost:8000/login", {
+      const response = await fetch("https://woc-yash-server.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
