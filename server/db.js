9ac0d24425c5589d3a30a761c8e6706da719ea8c
YashRathod123 <159521313+YashRathod123@users.noreply.github.com>
2024-02-09 13:59:03 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,11 +3,18 @@ const Pool = require("pg").Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    user:'postgres',
-    password:'123456789',
-    database:'IRCTC',
-    host:'localhost',
-    port:5432
+    // user:'postgres',
+    // password:'123456789',
+    // database:'IRCTC',
+    // host:'localhost',
+    // port:5432
+
+     connectionString:process.env.UserDatabase_URL,
+    ssl:true
 });
+
+pool.connect().then(()=>console.log('Connected'))
+.catch((err)=>(console.log('Error connecting from neels room',err)))
+
 
 module.exports=pool;
