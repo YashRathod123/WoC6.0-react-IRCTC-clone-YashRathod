77db76361b14302db3157aef42b9ce691f99b47e
YashRathod123 <159521313+YashRathod123@users.noreply.github.com>
2024-02-09 15:56:20 +0530

    Update payDetails.jsx

--- client/src/components/payDetails.jsx
+++ client/src/components/payDetails.jsx
@@ -23,7 +23,7 @@ function PayDetails({ log, tid }) {
       country,
     });
     try {
-      const response = await fetch("http://localhost:8000/book", {
+      const response = await fetch("https://woc-yash-server.onrender.com//book", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
